e41da7b53dc0d0ab4c8f12cda7953ad15bdeb43f
angelinaagelar <161010137+angelinaagelar@users.noreply.github.com>
2024-08-26 08:43:03 +0900

    Update TreeNode.java

--- src/main/java/family_tree/family_tree_Angelina/TreeNode.java
+++ src/main/java/family_tree/family_tree_Angelina/TreeNode.java
@@ -1,2 +1,21 @@
-package family_tree.family_tree_Angelina;public interface TreeNode {
+package family_tree.family_tree_Angelina;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TreeNode<T> extends Serializable, Iterable<T> {
+    void setId(long id);
+    long getId();
+    T getFather();
+    T getMother();
+    boolean addChild(T child);
+    boolean addParent(T parent);
+    String getName();
+    LocalDate getDeathDate();
+    LocalDate getBirthDate();
+    List<T> getParents();
+    List<T> getChildren();
+    T getSpouse();
+    void setSpouse(T spouse);
 }
