991be7f3b9b83f8581f61293a151ea0c874c3b76
angelinaagelar <161010137+angelinaagelar@users.noreply.github.com>
2024-08-26 08:44:34 +0900

    Update FamilyTreeModel.java

--- src/main/java/family_tree/model/FamilyTreeModel.java
+++ src/main/java/family_tree/model/FamilyTreeModel.java
@@ -1,2 +1,23 @@
-package family_tree.model;public class FamilyTreeModel {
+package family_tree.model;
+
+import family_tree.family_tree_Angelina.FamilyTree;
+import family_tree.human.Human;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class FamilyTreeModel implements Serializable {
+    private FamilyTree<Human> familyTree;
+
+    public FamilyTreeModel(List<Human> familyMembers) {
+        this.familyTree = new FamilyTree<>(familyMembers);
+    }
+
+    public FamilyTree<Human> getFamilyTree() {
+        return familyTree;
+    }
+
+    public void setFamilyTree(FamilyTree<Human> familyTree) {
+        this.familyTree = familyTree;
+    }
 }
