e16863a569ea94bf38148b82491fa0a15763304b
angelinaagelar <161010137+angelinaagelar@users.noreply.github.com>
2024-08-26 08:48:21 +0900

    Update FamilyTreeView.java

--- src/main/java/family_tree/view/FamilyTreeView.java
+++ src/main/java/family_tree/view/FamilyTreeView.java
@@ -1,2 +1,7 @@
-package family_tree.view;public interface FamilyTreeView {
+package family_tree.view;
+
+public interface FamilyTreeView {
+    void displayFamilyTree(String familyTreeInfo);
+    void showError(String message);
 }
+
